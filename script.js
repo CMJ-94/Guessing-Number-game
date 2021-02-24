035b0f22fd2ca10a4526e9bff931243f7759c1dc
CMJ-94 <78697574+CMJ-94@users.noreply.github.com>
2021-02-24 07:29:33 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Step 1: Generate random number between 0 and 9
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+// Step 2:This function calls each round to determine which guess is closest to target number
+const compareGuesses = (human, computer, target) => {
+  const humanGuess = Math.abs(human - target);
+  const computerGuess = Math.abs(computer - target);
+  if (humanGuess <= computerGuess) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Step 3: Function used to correctly increase the winner's score after each round
+const updateScore = string => {
+  if (string === 'human') {
+         humanScore += 1;
+     }else if (string === 'computer') {
+         computerScore += 1;
+     }
+}
+
+// Step 4: To updte the round number after each round 
+
+const advanceRound = () => {
+  currentRoundNumber += 1;
+}
+
+// https://gist.github.com/85e7bfa566a2cd0a39742ecab07e506a
+
